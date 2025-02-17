8b3543a0c04bf18b353735ea20de57c9e58816b0
mdsalmanfarsee <far90986@gmail.com>
2025-02-17 18:23:55 +0530

    use localstorage

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,16 +3,17 @@ import User from '../models/user.model.js';
 
 
 export const protectRoute = async (req, res, next) => {
-    const token = req.cookies.jwt;
+    //const token = req.cookies.jwt;
+    const token = localStorage.getItem("jwt");
     if (!token) {
         return res.status(401).json({ message: "You need to login first" });
     }
 
     try {
-        console.log(process.env.JWT_SECRET);
+        //console.log(process.env.JWT_SECRET);
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
+        //console.log(decoded);
         if (!decoded) {
             return res.status(401).json({ message: "Invalid token" })
         }
