d4a62b904e9459fc1608f64f96ee648677ff425a
mdsalmanfarsee <far90986@gmail.com>
2025-02-17 14:50:28 +0530

    link broke solve

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,8 +10,8 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //cookie cannot be accessed by client side script
-        secure: true, //cookie will only be sent over https connection vercel needed
-        sameSite: "None", //cookie is sent in all cross-origin requests
+        //secure: true, //cookie will only be sent over https connection
+        sameSite: "None", //cookie is sent in all cross-origin requests (vercel deployment)
     })
     return token;
 }
