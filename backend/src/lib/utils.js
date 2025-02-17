d05313fc42d39aa004d854326787fb8673599c89
mdsalmanfarsee <far90986@gmail.com>
2025-02-17 13:52:21 +0530

    bugs

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,6 +11,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //cookie cannot be accessed by client side script
         secure: true, //cookie will only be sent over https connection vercel needed
+        sameSite: "None", //cookie is sent in all cross-origin requests
     })
     return token;
 }
