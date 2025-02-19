9abc0813d2cf32267e6f6dc39a1e407a69642753
mdsalmanfarsee <far90986@gmail.com>
2025-02-19 18:21:04 +0530

    testing

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //cookie cannot be accessed by client side script
-        //secure: true, //cookie will only be sent over https connection
+        secure: true, //cookie will only be sent over https connection
         //sameSite: "None", //cookie is sent in all cross-origin requests (vercel deployment)
     });
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    //baseURL: 'http://localhost:3001/api',
-    baseURL: 'https://quick-chat-backend-murex.vercel.app/api',
+    baseURL: 'http://localhost:3001/api',
+    //baseURL: 'https://quick-chat-backend-murex.vercel.app/api',
     withCredentials: true,
 })
