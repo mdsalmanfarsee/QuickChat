111f6fa278d4800e4fa0cf8f1fdba8e8d155e1e2
mdsalmanfarsee <far90986@gmail.com>
2025-02-19 18:24:02 +0530

    testing

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //cookie cannot be accessed by client side script
         secure: true, //cookie will only be sent over https connection
-        sameSite: "None", //cookie is sent in all cross-origin requests (vercel deployment)
+        //sameSite: "None", //cookie is sent in all cross-origin requests (vercel deployment)
     });
 
     return token;
